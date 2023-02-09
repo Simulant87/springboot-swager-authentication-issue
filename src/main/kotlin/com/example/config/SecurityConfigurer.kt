febd816830436b4903ae79f5f6af6d7748f4baeb
Simulant <nfaupel.dev@gmail.com>
2023-02-09 15:03:25 +0100

    change working version to 2.7.8

--- src/main/kotlin/com/example/config/SecurityConfigurer.kt
+++ src/main/kotlin/com/example/config/SecurityConfigurer.kt
@@ -5,53 +5,29 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfigurer {
-
-    @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+class SecurityConfigurer : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
         http
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and().authorizeHttpRequests().requestMatchers(
-                // Open API Docs & UI
-                "/swagger-ui.html",
-                "/swagger-ui/**",
-                "/v3/api-docs/**",
-                // Health & live and readiness probes
-                "/actuator/health",
-            ).permitAll()
-            .and().authorizeHttpRequests().anyRequest().authenticated()
+            .and().authorizeRequests()
+            .anyRequest().authenticated()
+    }
 
-        return http.build()
+    override fun configure(web: WebSecurity) {
+        web.ignoring().antMatchers(
+            // Open API Docs & UI
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            // Health & live and readiness probes
+            "/actuator/health",
+        )
     }
 }
-
-// configuration for Spring Boot 2.7.8
-//@Configuration
-//@EnableWebSecurity
-//class SecurityConfigurer : WebSecurityConfigurerAdapter() {
-//    override fun configure(http: HttpSecurity) {
-//        http
-//            .csrf().disable()
-//            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//            .and().authorizeRequests()
-//            .anyRequest().authenticated()
-//    }
-//
-//    override fun configure(web: WebSecurity) {
-//        web.ignoring().antMatchers(
-//            // Open API Docs & UI
-//            "/swagger-ui.html",
-//            "/swagger-ui/**",
-//            "/v3/api-docs/**",
-//            // Health & live and readiness probes
-//            "/actuator/health",
-//        )
-//    }
-//}
